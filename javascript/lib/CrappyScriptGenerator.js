52e93a8414660344e9c8640955eac4076771e40e
Jeff Butler <jeffgbutler@gmail.com>
2017-03-31 08:06:56 -0400

    Updates from eslint

--- javascript/lib/CrappyScriptGenerator.js
+++ javascript/lib/CrappyScriptGenerator.js
@@ -1,45 +1,45 @@
-var XLSX = require('xlsx');
+const XLSX = require("xlsx");
 
-exports.generate = function () {
-    var applicationInformation = [
-        {
-            columnIndex: 1,
-            applicationId: 2237
-        },
-        {
-            columnIndex: 2,
-            applicationId: 4352
-        },
-        {
-            columnIndex: 3,
-            applicationId: 3657
-        },
-        {
-            columnIndex: 4,
-            applicationId: 5565
-        }
-    ];
-
-    function getInsertStatement(userId, applicationId) {
-        return "insert into ApplicationPermission(user_id, application_id) values('"
-            + userId
-            + "', "
-            + applicationId
-            + ");";
+const applicationInformation = [
+    {
+        columnIndex: 1,
+        applicationId: 2237
+    },
+    {
+        columnIndex: 2,
+        applicationId: 4352
+    },
+    {
+        columnIndex: 3,
+        applicationId: 3657
+    },
+    {
+        columnIndex: 4,
+        applicationId: 5565
     }
+];
 
+function getInsertStatement(userId, applicationId) {
+    return "insert into ApplicationPermission(user_id, application_id) values('"
+        + userId
+        + "', "
+        + applicationId
+        + ");";
+}
+
+function generate() {
     let lines = [];
-    let workbook = XLSX.readFile('lib/Users.xlsx');
+    let workbook = XLSX.readFile("lib/Users.xlsx");
     let worksheet = workbook.Sheets[workbook.SheetNames[0]];
     let sheetData = XLSX.utils.sheet_to_json(worksheet, { header: 1 }); // generate an array of arrays
     sheetData.forEach(function (row) {
         let userId = row[0];
         if (userId !== undefined) {
-            if (userId.substring(1, 2) === '.') {
+            if (userId.substring(1, 2) === ".") {
                 // valid user id
                 applicationInformation.forEach(function (appInfo) {
                     let value = row[appInfo.columnIndex];
-                    if (value === 'X') {
+                    if (value === "X") {
                         lines.push(getInsertStatement(userId, appInfo.applicationId));
                     }
                 });
@@ -48,3 +48,5 @@ exports.generate = function () {
     });
     return lines;
 }
+
+exports.generate = generate;
